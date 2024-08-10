cdf468f8f9a65b5d5bb0dee12b84529cf2bbe15c
Dmitry Petrov <166365976+pds05@users.noreply.github.com>
2024-08-10 17:46:37 +0400

    added threads pool

--- src/main/java/ru/otus/java/basic/http/server/Application.java
+++ src/main/java/ru/otus/java/basic/http/server/Application.java
@@ -2,6 +2,8 @@ package ru.otus.java.basic.http.server;
 
 public class Application {
     public static void main(String[] args) {
-        new HttpServer(8189).start();
+        HttpServer server = new HttpServer(8189);
+        server.start();
+        server.shutdown();
     }
 }
--- src/main/java/ru/otus/java/basic/http/server/HttpServer.java
+++ src/main/java/ru/otus/java/basic/http/server/HttpServer.java
@@ -3,10 +3,14 @@ package ru.otus.java.basic.http.server;
 import java.io.IOException;
 import java.net.ServerSocket;
 import java.net.Socket;
+import java.util.concurrent.ExecutorService;
+import java.util.concurrent.Executors;
 
 public class HttpServer {
+    public static final int REQUEST_HANDLER_THREADS = 10;
     private int port;
     private Dispatcher dispatcher;
+    private ExecutorService executorService;
 
     public HttpServer(int port) {
         this.port = port;
@@ -14,14 +18,39 @@ public class HttpServer {
     }
 
     public void start() {
+        executorService = Executors.newFixedThreadPool(REQUEST_HANDLER_THREADS);
         try (ServerSocket serverSocket = new ServerSocket(port)) {
             System.out.println("Сервер запущен на порту: " + port);
             do {
                 Socket socket = serverSocket.accept();
-                new HttpHandler(socket, dispatcher);
+                Thread request = new Thread(()->{
+                    try {
+                        byte[] buffer = new byte[8192];
+                        int n = socket.getInputStream().read(buffer);
+                        String rawRequest = new String(buffer, 0, n);
+                        HttpRequest httpRequest = new HttpRequest(rawRequest);
+                        httpRequest.printInfo(true);
+                        dispatcher.execute(httpRequest, socket.getOutputStream());
+                    } catch (Exception e) {
+                        e.printStackTrace();
+                    } finally {
+                        try {
+                            socket.close();
+                        } catch (IOException ex) {
+                            throw new RuntimeException(ex);
+                        }
+                    }
+                });
+                executorService.execute(request);
             } while (true);
         } catch (IOException e) {
             e.printStackTrace();
+        }
+    }
+
+    public void shutdown(){
+        if (executorService != null) {
+            executorService.shutdownNow();
         }
     }
 }
